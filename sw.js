1fc183b02a5211ab3441d960d453c1942593bc3b
adhilzee <153197478+adhilzee@users.noreply.github.com>
2026-06-04 19:23:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const STATIC_CACHE = "static";
-const DYNAMIC_CACHE = "dynamic-v27";
+const DYNAMIC_CACHE = "dynamic-v28";
 
 self.addEventListener("install", function (event) {
   console.log("Installed", event);
